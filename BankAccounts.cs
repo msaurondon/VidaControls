a218a9db71efeea790ea879b3a6a472afb545192
MJ Saurondon <msaurondon@gmail.com>
2015-02-10 14:28:07 -0500

    Initial
    Initial

---
+++ BankAccounts.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VidaControls
+{
+    public partial class BankAccounts: UserControl
+    {
+        public BankAccounts()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.dataGridView1.Rows.Add(comboBox1.SelectedItem.ToString(),
+                textBox1.Text,
+                textBox2.Text,
+                textBox3.Text,
+                textBox4.Text,
+                textBox5.Text);
+
+            this.dataGridView1.Refresh();
+            comboBox1.SelectedText = "";
+            foreach (Control t in this.Controls)
+            {
+                if (t.GetType() == typeof(TextBox))
+                {
+                    t.Text = String.Empty;
+                }
+            }
+
+        }
+    }
+}
