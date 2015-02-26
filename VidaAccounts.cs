3b3d1b0f471d7f54ef354c52a38c69a719580976
MJ Saurondon <msaurondon@gmail.com>
2015-02-26 15:56:00 -0500

    Add account data contract
    Beginning the data xfer process for the controls.

--- VidaAccounts.cs
+++ VidaAccounts.cs
@@ -3,22 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace VidaControls
 {
+    [DataContract]
     public class VidaAccounts
     {
+        [DataMember]
         public int AccountID { get; set; }
+        [DataMember]
         public int AccountTypeID { get; set; }
+        [DataMember]
         public string Institution { get; set; }
+        [DataMember]
         public int AccountNumber { get; set; }
+        [DataMember]
         public int RoutingNumber { get; set; }
+        [DataMember]
         public decimal Balance { get; set; }
+        [DataMember]
         public string AccountNickName { get; set; }
+        [DataMember]
         public decimal Limit { get; set; }
+        [DataMember]
         public decimal APR { get; set; }
+        [DataMember]
         public decimal MinimumPayment { get; set; }
+        [DataMember]
         public decimal MaximumPayment { get; set; }
+        [DataMember]
         public DateTime DueDate { get; set; }
     }
 }
