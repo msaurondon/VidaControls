20619c4257840943356815811464cc5edd98ab77
MJ Saurondon <msaurondon@gmail.com>
2015-02-26 14:52:23 -0500

    VitaAccountsClass added

---
+++ VidaAccounts.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VidaControls
+{
+    public class VidaAccounts
+    {
+        public int AccountID { get; set; }
+        public int AccountTypeID { get; set; }
+        public string Institution { get; set; }
+        public int AccountNumber { get; set; }
+        public int RoutingNumber { get; set; }
+        public decimal Balance { get; set; }
+        public string AccountNickName { get; set; }
+        public decimal Limit { get; set; }
+        public decimal APR { get; set; }
+        public decimal MinimumPayment { get; set; }
+        public decimal MaximumPayment { get; set; }
+        public DateTime DueDate { get; set; }
+
+    }
+}
