ae1b06f90fb88e5d9497f8493e3cfe32f2c4bb19
MJSaurondon <msaurondon@gmail.com>
2015-03-06 21:44:57 -0500

    How Much Auto

---
+++ VidaAutoEstimator.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace VidaControls
+{
+    partial class VidaAutoEstimator
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.APR = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.m_36 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.m_48 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.m_60 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.m_72 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.APR,
+            this.m_36,
+            this.m_48,
+            this.m_60,
+            this.m_72});
+            this.dataGridView1.Location = new System.Drawing.Point(28, 217);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(553, 314);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // APR
+            // 
+            this.APR.HeaderText = "APR";
+            this.APR.Name = "APR";
+            // 
+            // m_36
+            // 
+            this.m_36.HeaderText = "36 Months";
+            this.m_36.Name = "m_36";
+            // 
+            // m_48
+            // 
+            this.m_48.HeaderText = "48 Months";
+            this.m_48.Name = "m_48";
+            // 
+            // m_60
+            // 
+            this.m_60.HeaderText = "60 Months";
+            this.m_60.Name = "m_60";
+            // 
+            // m_72
+            // 
+            this.m_72.HeaderText = "72 Months";
+            this.m_72.Name = "m_72";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 174);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(148, 174);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(254, 174);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(360, 174);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(466, 174);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox6.Location = new System.Drawing.Point(42, 148);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 6;
+            this.textBox6.Text = "APR";
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox7.Location = new System.Drawing.Point(148, 148);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 7;
+            this.textBox7.Text = "36 Months";
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox8
+            // 
+            this.textBox8.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox8.Location = new System.Drawing.Point(254, 148);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 8;
+            this.textBox8.Text = "48 Months";
+            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox9
+            // 
+            this.textBox9.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox9.Location = new System.Drawing.Point(360, 148);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.ReadOnly = true;
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 9;
+            this.textBox9.Text = "60 Months";
+            this.textBox9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox10
+            // 
+            this.textBox10.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox10.Location = new System.Drawing.Point(466, 148);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.ReadOnly = true;
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 10;
+            this.textBox10.Text = "72 Months";
+            this.textBox10.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(135, 21);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(100, 20);
+            this.textBox11.TabIndex = 11;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(341, 21);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.ReadOnly = true;
+            this.textBox12.Size = new System.Drawing.Size(100, 20);
+            this.textBox12.TabIndex = 12;
+            this.textBox12.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(506, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Desired Payment";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(267, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Desired APR";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Desired Trade Value";
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(135, 47);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(100, 20);
+            this.textBox13.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Down payment";
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(135, 73);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(100, 20);
+            this.textBox14.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "State Taxes";
+            // 
+            // textBox15
+            // 
+            this.textBox15.Location = new System.Drawing.Point(135, 99);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.Size = new System.Drawing.Size(100, 20);
+            this.textBox15.TabIndex = 20;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(341, 50);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(86, 17);
+            this.checkBox1.TabIndex = 22;
+            this.checkBox1.Text = "Custom APR";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // VidaAutoEstimator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox15);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "VidaAutoEstimator";
+            this.Size = new System.Drawing.Size(610, 578);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn APR;
+        private System.Windows.Forms.DataGridViewTextBoxColumn m_36;
+        private System.Windows.Forms.DataGridViewTextBoxColumn m_48;
+        private System.Windows.Forms.DataGridViewTextBoxColumn m_60;
+        private System.Windows.Forms.DataGridViewTextBoxColumn m_72;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
---
+++ VidaAutoEstimator.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VidaControls
+{
+    public partial class VidaAutoEstimator : UserControl
+    {
+        int calculcateCustom = 0;
+        public VidaAutoEstimator()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.dataGridView1.Rows.Clear();
+            if (textBox11.Text.Length == 0) { MessageBox.Show("Need a desired payment"); }
+            else if (textBox12.ReadOnly == false) { calculcateCustom = 1; }
+            else { CalculateAmount(); }
+        }
+
+        private void CalculateAmount()
+        {
+            decimal payment = GetPayment();
+            decimal interest;
+            decimal totalInterest = 0;
+            decimal principal;
+            decimal totalPrincipal= 0;
+            decimal taxes = 0 ;
+            decimal tradeValue = GetTrade();
+            decimal downPayment = GetDownpayment();
+            decimal __36 = 0;
+            decimal __48 = 0;
+            decimal __60 = 0;
+            decimal __72 = 0;
+            decimal balance = 0;
+            decimal yearCount = 0;
+            decimal taxRate = GetTaxes();
+            
+            if (payment > 0) 
+            for (int APR = 15; APR >= 0; APR--)
+            {
+                decimal _apr = Convert.ToDecimal(APR) / 100;
+                decimal _mpr = _apr / 12;
+                if (taxRate != 0) { taxes = payment * (taxRate/100); }
+                for (int i = 1; i <= 72; i++)
+                {
+                    balance += payment;
+                    interest = balance * _mpr;
+                    principal = payment - interest;
+                    totalInterest += interest;
+                    totalPrincipal += principal;
+                    yearCount = (decimal)i / 12;
+                    if (yearCount == Convert.ToDecimal(3)) { __36 = ((totalPrincipal - (taxes * i)) + (tradeValue + downPayment)); }
+                    if (yearCount == Convert.ToDecimal(4)) { __48 = ((totalPrincipal - (taxes * i)) + (tradeValue + downPayment)); }
+                    if (yearCount == Convert.ToDecimal(5)) { __60 = ((totalPrincipal - (taxes * i)) + (tradeValue + downPayment)); }
+                    if (yearCount == Convert.ToDecimal(6)) { __72 = ((totalPrincipal - (taxes * i)) + (tradeValue + downPayment)); }
+
+                }
+
+                this.dataGridView1.Rows.Add(
+                    string.Format("{0}%", APR),
+                    string.Format("$ {0}", Math.Round(__36,2)),
+                    string.Format("$ {0}", Math.Round(__48,2)),
+                    string.Format("$ {0}", Math.Round(__60,2)),
+                    string.Format("$ {0}", Math.Round(__72, 2))
+                    );
+
+                totalInterest = 0;
+                totalPrincipal = 0;
+                balance = 0;
+                yearCount = 0;
+            }
+
+            this.dataGridView1.Refresh();
+        }
+
+        private decimal GetDownpayment()
+        {
+            decimal downPayment = 0;
+            if (textBox14.Text.Length > 0)
+            {
+                string downPaymentStr = textBox14.Text;
+                try
+                {
+                    downPayment = Convert.ToDecimal(downPaymentStr);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Down payment must be a decimal value");
+                    downPayment = 0;
+                }
+
+            }
+            return downPayment;
+        }
+
+        private decimal GetTrade()
+        {
+            decimal tradeValue = 0;
+            if (textBox13.Text.Length > 0)
+            {
+                string tradeValueStr = textBox13.Text;
+                try
+                {
+                    tradeValue = Convert.ToDecimal(tradeValueStr);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Trade Value must be a decimal value");
+                    tradeValue = 0;
+                }
+            }
+            return tradeValue;
+        }
+
+        private decimal GetPayment()
+        {
+            decimal payment = 0;
+            if (textBox11.Text.Length > 0)
+            {
+                string paymentStr = textBox11.Text;
+                try
+                {
+                    payment = Convert.ToDecimal(paymentStr);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Payment must be a decimal value");
+                    payment = 0;
+                }
+            }
+
+            return payment;
+        }
+
+        private decimal GetTaxes()
+        {
+            decimal taxes = 0;
+            if (textBox15.Text.Length > 0)
+            {
+                string taxesStr = textBox15.Text;
+                try
+                {
+                    taxes = Convert.ToDecimal(taxesStr);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Illegal number in taxes");
+                    taxes = 0;
+                }
+            }
+            return taxes;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                textBox12.ReadOnly = false;
+            }
+            else
+            {
+                textBox12.ReadOnly = true;
+            }
+        }
+
+    }
+}
