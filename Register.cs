3b33d9a489a5dea9c41f3f8aeb36e5c9d608d25c
MJSaurondon <msaurondon@gmail.com>
2015-03-08 21:47:38 -0400

    adding register

---
+++ Register.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VidaControls
+{
+    public class Register
+    {
+        DateTime Date { get; set; }
+        bool CLR { get; set; }
+        string Payee { get; set; }
+        string Memo { get; set; }
+        List<string> Categories { get; set; }
+        string SelectedCategory { get; set; }
+        decimal Deposit { get; set; }
+        decimal Withdraw { get; set; }
+        decimal Balance { get; set; }
+    }
+}
---
+++ VidaRegister.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace VidaControls
+{
+    partial class VidaRegister
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CLR = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Payee = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Memo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Category = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Deposit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Withdrawn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Balance = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Date,
+            this.CLR,
+            this.Payee,
+            this.Memo,
+            this.Category,
+            this.Deposit,
+            this.Withdrawn,
+            this.Balance});
+            this.dataGridView1.Location = new System.Drawing.Point(4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(856, 292);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Date
+            // 
+            this.Date.HeaderText = "Date";
+            this.Date.Name = "Date";
+            // 
+            // CLR
+            // 
+            this.CLR.HeaderText = "CLR";
+            this.CLR.Name = "CLR";
+            // 
+            // Payee
+            // 
+            this.Payee.HeaderText = "Payee";
+            this.Payee.Name = "Payee";
+            this.Payee.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Payee.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Memo
+            // 
+            this.Memo.HeaderText = "Memo";
+            this.Memo.Name = "Memo";
+            this.Memo.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Memo.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Category
+            // 
+            this.Category.HeaderText = "Category";
+            this.Category.Name = "Category";
+            // 
+            // Deposit
+            // 
+            this.Deposit.HeaderText = "Deposited";
+            this.Deposit.Name = "Deposit";
+            this.Deposit.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Deposit.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Withdrawn
+            // 
+            this.Withdrawn.HeaderText = "Withdrawn";
+            this.Withdrawn.Name = "Withdrawn";
+            this.Withdrawn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Withdrawn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Balance
+            // 
+            this.Balance.HeaderText = "Balance";
+            this.Balance.Name = "Balance";
+            this.Balance.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Balance.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // VidaRegister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "VidaRegister";
+            this.Size = new System.Drawing.Size(869, 310);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn CLR;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Payee;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Memo;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Category;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Deposit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Withdrawn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Balance;
+    }
+}
